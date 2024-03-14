33284789d82c422a5ecec48522704a6d5066efeb
BinyamMamo <binyammamo01@gmail.com>
2024-03-14 18:10:38 +0400

    add tuple create

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Task 7 - Creates a tuple with a string and a number.
+"""
+
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    returns a tuple of the string @k and the square of @v.
+    """
+    return k, v ** 2
+
