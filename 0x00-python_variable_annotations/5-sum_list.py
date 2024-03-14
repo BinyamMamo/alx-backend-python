ba95f72f7dbd4b19a2848d426f1191f8995697de
BinyamMamo <binyammamo01@gmail.com>
2024-03-14 17:45:43 +0400

    add comment

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 """
+Task 5 - sums contents of a list with floating typed values
 """
 
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """
+     returns the sum of all items in the list
+    """
     from functools import reduce
     return reduce(lambda x, y: x + y, input_list)
