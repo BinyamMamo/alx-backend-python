970c0b07023d207c9741ad140c60002bd9e623c6
BinyamMamo <binyammamo01@gmail.com>
2024-03-14 18:11:29 +0400

    fix formatting issues

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,12 +2,11 @@
 """
 Task 7 - Creates a tuple with a string and a number.
 """
-
 from typing import Union, Tuple
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     returns a tuple of the string @k and the square of @v.
     """
     return k, v ** 2
-
