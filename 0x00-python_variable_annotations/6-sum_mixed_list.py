22a35b60e52f8838c1d29e3f09b0280e987055e2
BinyamMamo <binyammamo01@gmail.com>
2024-03-14 17:57:17 +0400

    implement sum of a mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,10 +3,10 @@
 Task 6 - sums contents of a mixed list of integers and floats
 """
 
-from typing import List
+from typing import List, Union
 
 
-def sum_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
      returns the sum of all items in the list
     """
