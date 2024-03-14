2c7cb9f9c80ca611ebaed5b93cb7c143487b5d7d
BinyamMamo <binyammamo01@gmail.com>
2024-03-14 16:28:59 +0400

    implement string concat function with type annotated parameters

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Task 1 - concatenates two strings
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    returns a concatenated string
+    """
+    return str1 + str2
