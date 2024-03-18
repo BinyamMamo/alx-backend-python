8774fb8fb955717b19a282f7735a494125e40a47
BinyamMamo <binyammamo01@gmail.com>
2024-03-18 23:49:44 +0400

    fix formatting issues

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ from typing import List
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-     Create a task that invokes a function to spawn a delay n times 
+     Create a task that invokes a function to spawn a delay n times
     """
     wait_n = __import__('1-concurrent_coroutines').wait_n
     delays = await asyncio.create_task(wait_n(n, max_delay))
