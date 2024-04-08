4397cd2c1730a020d01f00d383e23b210e789b89
BinyamMamo <binyammamo01@gmail.com>
2024-04-08 18:58:21 +0400

    feat: add unit tests for get_json and memoize utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,8 @@ Write the first unit test for utils.access_nested_map
 """
 import unittest
 from parameterized import parameterized, parameterized_class
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -35,3 +36,57 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map raises KeyError for invalid paths."""
         with self.assertRaises(KeyError):
             access_nested_map(nest, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Class for testing get_json utility."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        """
+        Test get_json utility.
+
+        Args:
+            url: URL to fetch JSON from.
+            payload: Expected JSON payload.
+        """
+
+        class Mocked(Mock):
+            """Mocked response object."""
+
+            def json(self):
+                """Return mock JSON payload."""
+                return payload
+
+        with patch("requests.get") as MockClass:
+            """Mock requests.get call."""
+            MockClass.return_value = Mocked()
+            self.assertEqual(get_json(url), payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize tests memoize decorator"""
+
+    def test_memoize(self):
+        """test_memoize tests memoized functions are only called once"""
+
+        class TestClass:
+            """TestClass is a class for testing memoize"""
+
+            def a_method(self):
+                """a_method returns a constant"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """a_property returns result of a_method memoized"""
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mocked:
+            spec = TestClass()
+            spec.a_property
+            spec.a_property
+            mocked.asset_called_once()
